13ee3d6415ef91d1cc18d44a368f888afc1027ff
Sagar <sagar@Sagars-MacBook-Air.local>
2017-11-24 16:08:30 +0530

    Initial Commit

---
+++ JsonParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsonParsing
+//
+//  Created by Sagar on 24/11/17.
+//  Copyright © 2017 Sadyogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JsonParsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JsonParsing
+//
+//  Created by Sagar on 24/11/17.
+//  Copyright © 2017 Sadyogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
